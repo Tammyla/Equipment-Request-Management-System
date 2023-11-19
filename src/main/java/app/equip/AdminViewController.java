1ffd3bb0d6e1cef8604335bbe8bfb1475e47d9dc
Tammyla <101886558+Tammyla@users.noreply.github.com>
2023-11-18 22:33:31 -0600

    Added new GUI

--- src/main/java/app/equip/AdminViewController.java
+++ src/main/java/app/equip/AdminViewController.java
@@ -1,4 +1,26 @@
 package app.equip;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
 public class AdminViewController {
+
+    @FXML
+    private Button btnDashboard;
+
+    @FXML
+    private Button btnInventory;
+
+    @FXML
+    private Button btnRequest;
+
+    @FXML
+    private Button btnSignOut;
+
+    @FXML
+    private Button btnStock;
+
+    @FXML
+    private Button btnUser;
+
 }
--- src/main/java/app/equip/GuestLoginViewController.java
+++ src/main/java/app/equip/GuestLoginViewController.java
@@ -2,6 +2,7 @@ package app.equip;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 
@@ -14,18 +15,21 @@ public class GuestLoginViewController {
     private Button btnLogin;
 
     @FXML
+    private ComboBox<?> cbxRoles;
+
+    @FXML
     private TextField txtEmail;
 
     @FXML
     private TextField txtName;
 
     @FXML
-    private TextField txtPassword;
+    private PasswordField txtPassword;
 
     @FXML
-    private PasswordField txtPhoneNo;
+    private TextField txtPhoneNo;
 
     @FXML
-    private PasswordField txtUsername;
+    private TextField txtUsername;
 
 }
